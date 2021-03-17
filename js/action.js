a17370dcc0e18a492a141d911489b3af8b9dab48
Shubam Bhasin <33823641+shubambhasin@users.noreply.github.com>
2021-03-17 16:24:06 +0530

    fix: click to copy fixed
    Removed nav
    -refactored code, removed unwanted css, js

--- js/action.js
+++ js/action.js
@@ -1,33 +1,8 @@
-// alert("connected");
-// Used jquery v3.5.1 and font-awesome v4.7.0
-$(document).ready(function() {
-    $('.nav .dropdown-menu').prev('a').on('click', function(e) {
-      e.preventDefault();
-      $(this).parent().find('.dropdown-menu').slideToggle();
-    });
-});
+var clipboard = new ClipboardJS(".btn");
 
-
-
-
-
-var clipboard = new ClipboardJS('.btn');
-
-clipboard.on('success', function(e) {
-    console.info('Action:', e.action);
-    console.info('Text:', e.text);
-    console.info('Trigger:', e.trigger);
-
-    e.clearSelection();
-
-    e.trigger.textContent = 'Copied!';
-});
-
-clipboard.on('error', function(e) {
-    console.error('Action:', e.action);
-    console.error('Trigger:', e.trigger);
-});
-
-
-
-
+      clipboard.on("success", function (e) {
+        e.trigger.textContent = "Copied! 😄";
+        setTimeout(() => {
+          e.trigger.textContent = e.text;
+        }, 1000);
+      });
